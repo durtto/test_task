010194d0f2a6b29546ec32bb3feebbfaa6ac68c5
vitaliy <vitaliylevitskiand@gmail.com>
2017-07-04 19:04:58 +0300

    tmp

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 import json
+import time
 from threading import Thread
 import requests
-import lxml
 from lxml import html
-from io import StringIO
-from lxml import etree
 from flask import Flask, render_template
 from flask_socketio import SocketIO, send
+
+odds = {}
+interval = 1
 
 
 class Odd():
@@ -15,6 +16,11 @@ class Odd():
         self.value = value  # example: 1.4, 1.5
         self.print_value = print_value  # example: 1/4, 2/7
         self.probability = probability
+
+    def to_dict(self):
+        return {'name': self.name, 'value': self.value,
+                'print_value': self.print_value,
+                'probability': self.probability}
 
 
 class BaseMarketParser:
@@ -28,23 +34,44 @@ class BaseMarketParser:
         """
         :return dict {Odd.name: Odd}
         """
-        raise NotImplementedError
+        return {}
 
 
 def calculate_probabilities(odds):
-    sum = reduce(lambda odd: 1 / odd.value, odds)
+    sum = reduce(lambda sum, odd: 1 / odd['value'], odds)
     for odd in odds:
-        odd.probability = 1 / odd.value / sum
+        odd['probability'] = 1 / odd['value'] / sum
 
 
 # Parsers impl
 class SkyBet(BaseMarketParser):
-    pass
+    @property
+    def name(self):
+        return "SkyBet"
 
 
 class Bet365(BaseMarketParser):
     url = 'https://www.bet365.com/?lng=1&amp;cb=105812028182#/AC/B2/C101/' \
           'D20170703/E20537844/F65742225/G1/P11/'
+
+    @property
+    def name(self):
+        return "Bet365"
+
+
+class PaddyPower(BaseMarketParser):
+    @property
+    def name(self):
+        return "PaddyPower"
+
+
+class WilliamHill(BaseMarketParser):
+    url = 'http://sports.williamhill.com/bet/en-gb/results///E/11336004/' \
+          'thisDate/2017/07/03/17:00:00//5%3a00+Pontefract.html'
+
+    @property
+    def name(self):
+        return "WilliamHill"
 
     def parse(self):
         page = requests.get(self.url)
@@ -52,16 +79,21 @@ class Bet365(BaseMarketParser):
             return False
         tree = html.fromstring(page.content)
         horses = tree.xpath(
-            '//span[@class="rl-HorseTrainerJockey_Horse"]/text()')
-        print horses
-
-
-class PaddyPower(BaseMarketParser):
-    pass
-
-
-class WilliamHill(BaseMarketParser):
-    pass
+            '//table[@class="md_runnerDetails md_rd_template5"]//'
+            'td[@class="md_runner"]/text()')
+        scores = tree.xpath(
+            '//table[@id="meetingData_results"]//td[@class="md_column5"]/'
+            'strong/text()')
+        horses = [h.strip() for h in horses if h.strip()]
+        scores = [h.strip() for h in scores]
+        result = {}
+        for horse, value in zip(horses, scores):
+            print_value = value
+            odd, to = print_value.split('/')
+            real_value = float(odd) / int(to)
+            result[horse] = Odd(horse, real_value, print_value, 0).to_dict()
+        calculate_probabilities(result.values())
+        return result
 
 
 parsers = [SkyBet(), Bet365(), PaddyPower(), WilliamHill()]
@@ -71,7 +103,7 @@ def get_odds():
     odds = {}
     for parser in parsers:
         site_odds = parser.parse()
-        for odd, value in site_odds:
+        for odd, value in site_odds.items():
             if not odds.get(odd):
                 odds[odd] = {parser.name: value}
             else:
@@ -80,9 +112,11 @@ def get_odds():
 
 
 def parse_loop():
+    global odds
     while True:
-        new_data = get_odds()
-        send(json.dumps(new_data), json=True, namespace='/update')
+        odds = get_odds()
+        #send(json.dumps(odds), json=True, namespace='/update', broadcast=True)
+        time.sleep(interval)
 
 
 app = Flask(__name__, template_folder='.')
@@ -92,7 +126,6 @@ socketio = SocketIO(app)
 
 @app.route('/')
 def index():
-    odds = get_odds()
     sites = [parser.name for parser in parsers]
     return render_template('index.html', odds=odds, sites=sites)
 
@@ -100,5 +133,5 @@ def index():
 parse_thread = Thread(target=parse_loop)
 
 if __name__ == '__main__':
-    # parse_thread.start()
+    parse_thread.start()
     socketio.run(app)
--- test.py
+++ test.py
@@ -1,8 +1,7 @@
 import requests
 from lxml import html
 
-url = 'https://www.bet365.com/?lng=1&amp;cb=105812028182#/AC/B73/C104/' \
-      'D20170703/E20537884/F65746428/G1/P11/'
+url = 'http://sports.williamhill.com/bet/en-gb/results///E/11336004/thisDate/2017/07/03/17:00:00//5%3a00+Pontefract.html'
 
 
 def parse():
@@ -10,10 +9,15 @@ def parse():
     if page.status_code != 200:
         return False
     tree = html.fromstring(page.content)
-    horses = tree.xpath('//span[@class="rl-HorseTrainerJockey_Horse"]')
-    print horses
+    horses = tree.xpath(
+        '//table[@class="md_runnerDetails md_rd_template5"]//td[@class="md_runner"]/text()')
+    scores = tree.xpath(
+        '//table[@id="meetingData_results"]//td[@class="md_column5"]/strong/text()')
+    horses = [h.strip() for h in horses if h.strip()]
+    scores = [h.strip() for h in scores]
+    return zip(horses, scores)
 
 
-parse()
-
-
+r = parse()
+for horse, score in iter(r):
+    print '%s - %s' % (horse, score)
