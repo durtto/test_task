5c2fa287d7a734e03793e970ef78d73df18d3116
vitaliy <vitaliylevitskiand@gmail.com>
2017-07-05 18:41:58 +0300

    tmp

--- test.py
+++ test.py
@@ -1,23 +1,22 @@
 import requests
 from lxml import html
+import json
+import re
 
-url = 'http://sports.williamhill.com/bet/en-gb/results///E/11336004/thisDate/2017/07/03/17:00:00//5%3a00+Pontefract.html'
+url = 'http://www.paddypower.com/racing/horse-racing/deauville/11%3a25-STAKES-6f-101yds-12518007.html'
 
 
 def parse():
     page = requests.get(url)
     if page.status_code != 200:
         return False
-    tree = html.fromstring(page.content)
-    horses = tree.xpath(
-        '//table[@class="md_runnerDetails md_rd_template5"]//td[@class="md_runner"]/text()')
-    scores = tree.xpath(
-        '//table[@id="meetingData_results"]//td[@class="md_column5"]/strong/text()')
-    horses = [h.strip() for h in horses if h.strip()]
-    scores = [h.strip() for h in scores]
-    return zip(horses, scores)
-
-
-r = parse()
-for horse, score in iter(r):
-    print '%s - %s' % (horse, score)
+    m = re.search('hr_ev_init\((.*),"",\'!!!!!!!!!',
+                  page.content.replace('\n', ''))
+    if m:
+        found = m.group(1)
+        found = "[%s]" % found
+        js = json.loads(found)
+        for odd in js[3]:
+            if not odd['lp_den']: continue
+            print '%s - %s/%s' % (
+                odd['names']['en'], odd['lp_num'], odd['lp_den'])
